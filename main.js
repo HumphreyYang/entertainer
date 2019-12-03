149f6b2f3da1ea35411b8cfc95f965f02ae5fae8
NeilKleistGao <NeilKleistGao@163.com>
2019-12-03 23:29:57 +0800

    add mongodb initialization

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
 let express = require("express");
 let app = express();
 
+let client = require("mongodb").MongoClient;
+let db_url = "mongodb://localhost:27017";
+
+client.connect(db_url,{ useUnifiedTopology: true }, function (err, db) {
+    if (err) {
+        throw err;
+    }
+
+    let database = db.db("entertainer");
+    let flag = false;
+    database.listCollections({name: "joplin"}).next(function (err, collinfo) {
+        if (collinfo) {
+            flag = true;
+        }
+    });
+
+    if (!flag) {
+        database.createCollection("joplin", function (err, res) {
+            if (err) {
+                throw err;
+            }
+        });
+
+        database.createCollection("works", function (err, res) {
+            if (err) {
+                throw err;
+            }
+        });
+
+        let init = {password: "1234567"};
+        database.collection("joplin").insertOne(init,function (err, res) {
+            if (err) {
+                throw err;
+            }
+
+            console.log("initialization finished.");
+            db.close();
+        });
+    }
+});
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
